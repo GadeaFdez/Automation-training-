bdc23e1dbbc15fc49af2569dc957e144715813d1
Gadea Fernandez <gadeafernandez@Gadeas-MacBook-Pro-2.local>
2023-12-01 10:33:58 +0000

    Changing files location

--- JS challenges/data_type_one.js
+++ JS challenges/data_type_one.js
@@ -1,11 +1,7 @@
-let my_num = 2; //define a variable and assign a number to it 
+let my_num = 2; 
 
-console.log(my_num); //printing the value of the number variable
+let my_bool = 1 > 5; 
 
-let my_bool = 1 > 5; //define a variable and assign a boolean type to it 
+console.log(my_bool); 
 
-console.log(my_bool); //printing the value of the boolean variable
-
-let my_str = "this is a string"; // //define a variable and assign a string to it 
-
-console.log(my_str); //printing the value of the string variable
+let my_str = "this is a string"; 
