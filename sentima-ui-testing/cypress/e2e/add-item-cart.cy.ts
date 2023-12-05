b4a0a238636d37b77dc8ec4948ec0122fb25d1a1
Gadea Fernandez <gadeafernandez@Gadeas-MBP-2.cust.communityfibre.co.uk>
2023-12-05 15:59:30 +0000

    Adding baseUrl config

--- sentima-ui-testing/cypress/e2e/add-item-cart.cy.ts
+++ sentima-ui-testing/cypress/e2e/add-item-cart.cy.ts
@@ -1,46 +1,54 @@
+import * as selectors from '../page-object-model/selectors'; 
+
+const email: string = "gfn@gmail.com"; 
+const country : string = "Spain"; 
+const firstName: string = "ghj"; 
+const lastName: string = "fn"; 
+const addressInput: string = "241"; 
+const dressName: string = "Sofia Dress"; 
+
+
 describe('Adds a product to the cart and checks out', () => {
 
   beforeEach(() => {
     Cypress.on('uncaught:exception', (err, runnable) => {
-      // returning false here prevents Cypress from
-      // failing the test
+      
       console.log('Cypress detected uncaught exception: ', err);
       return false
     }); 
+    cy.visit('/') //visits page  
   })
   
   
-  it('visits sentima website and an item to the cart ', () => {
-    cy.visit('https://sentimaco.com/'); //visits page  
-    cy.get('#title-template--21049109610782__featured_collection-8628356284702').click(); // chooses a dress 
-    cy.get('.PwzrButton-label-514').click(); // chooses a size 
-    cy.get('#ProductSubmitButton-template--21049110102302__main').click(); //adds choice to the cart
-    cy.get('.styles_ProductRow__productTitleAndVariant__On413').should('contain', "Sofia Dress"); //asserts that the dress has been added to the cart 
-    cy.get('.upcart-checkout-button-container').click(); 
-    cy.get('[data-testid="proceed"]').click(); 
-
-    
+  it.only('visits sentima website and an item to the cart ', () => {
+    cy.get(selectors.SOFIA_DRESS_HOMEPAGE).click(); // chooses a dress 
+    cy.get(selectors.MEDIUM_SIZE).click(); // chooses a size 
+    cy.get(selectors.ADD_TO_CART).click(); //adds choice to the cart
+    cy.get(selectors.CART_LIST).should('contain', dressName); //asserts that the dress has been added to the cart 
+    cy.get(selectors.SHIPPING_PROTECTION_TOGGLE).click(); 
+    cy.get(selectors.CART_CHECKOUT).click(); 
+    cy.get(selectors.UPSELL_MODAL_CONTINUE).click(); 
   })
 
-it.only('fills up basic details in cart', () => {
-
-  cy.visit('https://sentimaco.com/'); //visits page  
-  cy.get('#title-template--21049109610782__featured_collection-8628356284702').click(); // chooses a dress 
-  cy.get('.PwzrButton-label-514').click(); // chooses a size 
-  cy.get('#ProductSubmitButton-template--21049110102302__main').click(); //adds choice to the cart
-  cy.get('.styles_ProductRow__productTitleAndVariant__On413').should('contain', "Sofia Dress"); //asserts that the dress has been added to the cart 
-  cy.get('.upcart-checkout-button-container').click(); 
-  cy.get('[data-testid="proceed"]').click(); 
-  cy.get('#email').type("gfn@gmail.com"); 
-  cy.get('#Select0').select('Spain')
-  cy.get('#TextField10').type('ghj'); 
-  cy.get('#TextField11').type('fn'); 
-  cy.get('#shipping-address1').type('241'); 
+  it('fills up basic details in cart', () => {
+  cy.get(selectors.SOFIA_DRESS_HOMEPAGE).click(); // chooses a dress 
+  cy.wait(2000); 
+  //cy.get(selectors.SOFIA_DRESS_PRICE); 
+  cy.get(selectors.MEDIUM_SIZE).click(); // chooses a size 
+  cy.get(selectors.ADD_TO_CART).click(); //adds choice to the cart
+  cy.get(selectors.CART_LIST).should('contain', dressName); //asserts that the dress has been added to the cart 
+  
+  cy.get(selectors.CART_CHECKOUT).click(); 
+  cy.get(selectors.UPSELL_MODAL_CONTINUE).click(); 
+  cy.get(selectors.EMAIL_INPUT_CHECKOUT).type(email); 
+  cy.get(selectors.COUNTRY_DROPDOWN_CHECKOUT).select(country)
+  cy.get(selectors.FIRST_NAME_INPUT_CHECKOUT).type(firstName); 
+  cy.get(selectors.LAST_NAME_INPUT_CHECKOUT).type(lastName); 
+  cy.get(selectors.ADDRESS_DROPDOWN_CHECKOUT).type(addressInput); 
   cy.wait(1000); 
-  cy.get('#shipping-address1-option-1').click();
-    
+  cy.get(selectors.FULL_ADDRESS_CHECKOUT).click();
+  //cy.get(selectors.TOTAL_PRICE_CHECKOUT).should('eq', '@price'); 
 })
     
-
-
 })
+
